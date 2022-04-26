921e45bbb6715bef8ab22bb3fdef73270dce0259
bhanderijanvi415 <bhanderijanvi415@gmail.com>
2022-04-26 09:50:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dicegame/playgame.java
@@ -0,0 +1,32 @@
+package com.example.dicegame;
+
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Locale;
+import java.util.Random;
+
+
+class playgame extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
